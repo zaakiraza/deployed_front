57ef6fd6a2dcc9841bb5b3e4d9cad2713b018e9e
zaakiraza <raaza.zaakir@gmail.com>
2025-08-04 21:33:06 +0500

    check

--- src/web/components/AboutUS/AboutUs.jsx
+++ src/web/components/AboutUS/AboutUs.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import { Box, Flex, Heading, Text, Image, Stack } from '@chakra-ui/react'
-import HeroSection2 from '../ContactUS/HeroSection2'
-import HistorySection from './HistorySection'
-import AimSection from './AimSection'
-import ProgressSection from './ProgressSection'
-import CampusTourSection from './CampusTourSection'
-import FeedbackSection from './Feedback/FeedbackSection'
-import LetsStartWithOTS from '../landingpage/Let\'s-Start-With-OTS'
-import Footer from '../landingpage/Footer'
-import BOX from '../../views/BOX'
-import ScrollToTopButton from '../ScrollToTopButton'
+import React from "react";
+import { Box, Flex, Heading, Text, Image, Stack } from "@chakra-ui/react";
+// import HeroSection2 from '../ContactUS/HeroSection2'
+import HeroSection2 from "../ContactUs/HeroSection2";
+import HistorySection from "./HistorySection";
+import AimSection from "./AimSection";
+import ProgressSection from "./ProgressSection";
+import CampusTourSection from "./CampusTourSection";
+import FeedbackSection from "./Feedback/FeedbackSection";
+import LetsStartWithOTS from "../landingpage/Let's-Start-With-OTS";
+import Footer from "../landingpage/Footer";
+import BOX from "../../views/BOX";
+import ScrollToTopButton from "../ScrollToTopButton";
 
 function AboutUs() {
   return (
     <BOX>
       <HeroSection2>
-                <Text
-                  as="span"
-                  bg="#F49040"
-                  display="inline-block"
-                  paddingY={{ base: "4px", sm: "8px", md: "10px" }}
-                  paddingX={{ base: "8px", sm: "12px", md: "16px" }}
-                   mr={{ base: "4px", sm: "8px", md: "12px" }}
-                >
-                  About
-                </Text>
-                 Us
+        <Text
+          as="span"
+          bg="#F49040"
+          display="inline-block"
+          paddingY={{ base: "4px", sm: "8px", md: "10px" }}
+          paddingX={{ base: "8px", sm: "12px", md: "16px" }}
+          mr={{ base: "4px", sm: "8px", md: "12px" }}
+        >
+          About
+        </Text>
+        Us
       </HeroSection2>
       <HistorySection />
       <AimSection />
@@ -34,9 +35,8 @@ function AboutUs() {
       <FeedbackSection />
       <Footer />
       <ScrollToTopButton />
-
     </BOX>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
